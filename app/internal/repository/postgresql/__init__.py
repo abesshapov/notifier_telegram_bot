53cfd778f8976a4773ecbe857eed43724515eff6
dubaimydream111 <dubaimydream212121@proton.me>
2024-08-01 21:58:02 +0300

    added DI reference to repositories

--- app/internal/repository/postgresql/__init__.py
+++ app/internal/repository/postgresql/__init__.py
@@ -1,7 +1,16 @@
 """All postgresql repositories are defined here."""
 
 from dependency_injector import containers, providers
+from app.internal.repository.postgresql.notes import NoteRepository
+from app.internal.repository.postgresql.users import UserRepository
 
 
 class Repositories(containers.DeclarativeContainer):
     """Container for postgresql repositories."""
+
+    notes_repository: NoteRepository = providers.Singleton(
+        NoteRepository,
+    )
+    users_repository: UserRepository = providers.Singleton(
+        UserRepository,
+    )
