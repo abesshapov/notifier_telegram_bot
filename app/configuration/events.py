fcd88520677ba5da1f40ff19ea6556f6a68b5af7
dubaimydream111 <dubaimydream212121@proton.me>
2024-08-02 01:41:15 +0300

    added notifier worker

--- app/configuration/events.py
+++ app/configuration/events.py
@@ -1,4 +1,6 @@
 """``on_startup`` function will be called when server trying to start."""
+
+import asyncio
 from contextlib import asynccontextmanager
 
 from dependency_injector.wiring import Provide, inject
@@ -6,12 +8,14 @@ from fastapi import FastAPI
 
 from app.internal.services import Services
 from app.internal.services.telegram import TelegramService
+from app.internal.workers import NotifierWorker, Workers
 
 
 @asynccontextmanager
 @inject
 async def lifespan(
     app: FastAPI,  # pylint: disable=unused-argument # noqa: F841
+    notifier_worker: NotifierWorker = Provide[Workers.notifier_worker],
     telegram_service: TelegramService = Provide[Services.telegram_service],
 ) -> None:  # type: ignore
     """Run code on server startup.
@@ -25,5 +29,6 @@ async def lifespan(
     """
 
     await telegram_service.set_webhook()
+    asyncio.shield(notifier_worker.run())
     yield
     await telegram_service.close_session()
